a958578e924e8da87dd64122d765c09e0f6b835a
Manthan Patelgit config --global user.email patelmanthan23507@gmail.comgit config --listgit config --global --listgit config --global user.name Manthan <patelmanthan23507@gmail.com>
2026-08-09 13:54:56 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/hacthon/ai_rti_assistant/AiRtiAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.hacthon.ai_rti_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRtiAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRtiAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hacthon/ai_rti_assistant/AiRtiAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacthon.ai_rti_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRtiAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
